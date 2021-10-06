a2aa19e23d7d7d794886981a7ba4bd4a22368ce3
AudriusAnusauskas <aaaudrius@gmail.com>
2021-10-06 22:07:45 +0300

    sand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarehouseComponent } from './warehouse/warehouse.component';
+import { ProductsComponent } from './warehouse/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarehouseComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warehouse/products/products.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
+})
+export class ProductsComponent implements OnInit {
+  pridetiVisible=false;
+  @Input() products;
+  @Output() orderEmitter = new EventEmitter<{ name: string }>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  order(name: string) {
+    console.log('Uzsakoma: ' + name);
+    this.orderEmitter.emit({ name: name });
+  }
+
+  addProduct(
+    inputPavadinimas: HTMLInputElement,
+    inputKaina: HTMLInputElement,
+    inputKiekis: HTMLInputElement,
+    inputRezervuota: HTMLInputElement
+  ) {
+    console.log(
+      inputPavadinimas.value +
+        ' ' +
+        inputKaina.value +
+        ' ' +
+        inputKiekis.value +
+        ' ' +
+        inputRezervuota.value
+    );
+    this.products.push({
+      name: inputPavadinimas.value,
+      price: parseFloat(inputKaina.value),
+      count: parseInt(inputKiekis.value),
+      reserved: parseInt(inputRezervuota.value),
+    });
+    console.log(this.products);
+    inputPavadinimas.value = "";
+  }
+}
